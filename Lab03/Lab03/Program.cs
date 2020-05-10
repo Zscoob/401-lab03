341fbaf9cc37ab2e318f76227a22463eb7e3cecb
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-09 20:05:07 -0500

    added try/catch and an error log

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -12,6 +12,11 @@ namespace Lab03
             string[] starterItem = new string[] { "nutella" };
             File.WriteAllLines(filePath, starterItem);
 
+            //setting up error log
+            string errorFilePath = @"Assets/errorLog.txt";
+            string[] errorStart = new string[] { "Error log:" };
+            File.WriteAllLines(errorFilePath, errorStart);
+
             bool displayMenu = true;
             while (displayMenu == true)
             {
@@ -57,6 +62,12 @@ namespace Lab03
             {
                 return false;
             }
+            else if (result == "error")
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                Console.WriteLine(ViewList(errorFilePath));
+                return true;
+            }
             else
             {
                 return false;
@@ -66,43 +77,103 @@ namespace Lab03
         
         public static string ViewList(string input)
         {
-            
 
-            string[] listArray = File.ReadAllLines(input);
 
-            //Converts Array into one string.
-            string response = "";
-            foreach(string foodItem in listArray)
+            try
             {
-                response += $"{foodItem}, ";
-            }
+                string[] listArray = File.ReadAllLines(input);
 
-            //takes off trailing whitespace
-            response = response.Trim();
-          
-            return response;
+                //Converts Array into one string.
+                string response = "";
+                foreach (string foodItem in listArray)
+                {
+                    response += $"{foodItem}, ";
+                }
+
+                //takes off trailing whitespace
+                response = response.Trim();
+
+                return response;
+            }
+            catch (DirectoryNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+                return "Invalid Path";
+            }
+            catch (FileNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+                return "File not found";
+            }
+            catch (IOException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+                return "System IO error";
+            }
         }
         public static void AddItem(string path, string content)
         {
-             
-            string[] newItem = new string[] { content };
-           
-            File.AppendAllLines (path, newItem);
-       
 
-            Console.WriteLine("item added");
-
-          
+            try
+            {
+                string[] newItem = new string[] { content };
+                File.AppendAllLines(path, newItem);
+                Console.WriteLine("item added");
+            }
+            catch (DirectoryNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
+            catch (FileNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
+            catch (IOException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
 
 
         }
         public static void DelItem(string path, string itemToBeNotEaten)
         {
-            string[] intialArray = File.ReadAllLines(path);
-            string[] filteredArray = Array.FindAll(intialArray, nomnom => nomnom != itemToBeNotEaten);
-            //Console.WriteLine(String.Join(", ", File.ReadAllLines(path)));
-            File.WriteAllLines(path, filteredArray);
-            
+            try
+            {
+                string[] intialArray = File.ReadAllLines(path);
+                string[] filteredArray = Array.FindAll(intialArray, nomnom => nomnom != itemToBeNotEaten);
+                File.WriteAllLines(path, filteredArray);
+            }
+            catch (DirectoryNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
+            catch (FileNotFoundException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
+            catch (IOException error)
+            {
+                string errorFilePath = @"Assets/errorLog.txt";
+                string errorString = $"{error}";
+                AddItem(errorFilePath, errorString);
+            }
+
         }
     }
 }
